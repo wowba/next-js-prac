cdd3d23fbe11c6e9b97fbc7674cde6b22e8e5865
wowba <leeyuwk54@gmail.com>
2023-05-02 02:23:13 +0900

    next-sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://next-js-prac-delta.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
